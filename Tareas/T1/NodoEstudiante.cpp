2000d27ac261be17782ad8e6fc6f22caff362cdf
Vernik <3003160030101@ingenieria.usac.edu.gt>
2020-08-02 07:50:08 -0600

    actualizacion T1

--- Tareas/T1/NodoEstudiante.cpp
+++ Tareas/T1/NodoEstudiante.cpp
@@ -2,6 +2,31 @@
 #include "NodoEstudiante.h"
 #include <iostream>
 
-NodoEstudiante::NodoEstudiante() {
+NodoEstudiante::NodoEstudiante(int carne, string nombre) {
+	this->carne = carne;
+	this->nombre = nombre;
+	this->siguiente = NULL;
+}
+
+string NodoEstudiante::getNombre() {
+	return this->nombre;
+}
+
+int NodoEstudiante::getCarne() {
+	return this->carne;
+}
+
+NodoEstudiante* NodoEstudiante::getSiguiente() {
+	return this->siguiente;
+}
+
+void NodoEstudiante::imprimir() {
+	cout << "Carne: " << this->getCarne() << " Nombre: " << this->getNombre()<<endl;
+}
+
+void NodoEstudiante::setSiguiente(NodoEstudiante* sig) {
+	this->siguiente = sig;
+}
+NodoEstudiante::~NodoEstudiante() {
 
 }
